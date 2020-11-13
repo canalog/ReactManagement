56dfc2b24e64c3f899037426f3c93930a57502d3
Alice Cha <alicecha719@naver.com>
2020-11-13 23:31:04 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import logo from './logo.svg';
+import Customer from './components/Customer';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <img src={logo} alt="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: '홍길동',
+    birthday: '961222',
+    gender: '남자',
+    job: '대학생'
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '홍제니',
+    birthday: '961222',
+    gender: '여자',
+    job: '의사'
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: '김진환',
+    birthday: '970212',
+    gender: '남자',
+    job: '가수'
+  }
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(customer => {
+            return (
+              <Customer 
+                key={customer.id}
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
